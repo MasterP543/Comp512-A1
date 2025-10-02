b3854031b58de5b34c043fab29e1ccaacee52a8d
MasterP543 <perrysantry17@gmail.com>
2025-10-02 12:42:27 -0400

    NOW its fixed

--- Server/rmi_run_servers.sh
+++ Server/rmi_run_servers.sh
@@ -18,4 +18,4 @@ tmux new-session \; \
   select-pane -t 3 \; \
   send-keys "ssh -t ${MACHINES[2]} \"cd $(pwd) > /dev/null; ./run_rmi.sh; echo -n 'Connected to '; hostname; ./rmi_run_server.sh Rooms\"" C-m \; \
   select-pane -t 0 \; \
-  send-keys "ssh -t ${MACHINES[4]} \"cd $(pwd) > /dev/null; ./run_rmi.sh; echo -n 'Connected to '; hostname; sleep .5s; ./rmi_run_middleware.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]}\"" C-m \;
+  send-keys "ssh -t ${MACHINES[3]} \"cd $(pwd) > /dev/null; ./run_rmi.sh; echo -n 'Connected to '; hostname; sleep .5s; ./rmi_run_middleware.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]}\"" C-m \;
